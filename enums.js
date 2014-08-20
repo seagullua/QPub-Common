1c018b119fd4fba432e1d164dd3efdf524e317b7
Andriy Chaika <andriy@chaika.in.ua>
2014-08-20 13:35:47 +0300

    Initial commit

---
+++ enums.js
@@ -0,0 +1,103 @@
+var QType = {
+    Size: "Size",
+    Bool: "Bool",
+    Font: "Font",
+    Color: "Color",
+    FontStyle: "FontStyle", //Regular, Italic e.t.c
+    PointSize: "PointSize"
+}
+
+var QTags = {
+    P: "p",
+    Image: "image",
+    Title: "title"
+}
+
+var QParagraphStyles = {
+    Paragraph: "qpub_p"
+
+}
+
+var QCharacterStyles = {
+    Underline: "qpub_u",
+    Bold: "qpub_b",
+    Italic: "qpub_i"
+}
+
+var QLanguage = {
+    UK_UA: "uk_UA",
+    RU_RU: "ru_RU",
+    EN_US: "en_US",
+    EN_UK: "en_UK"
+}
+
+var QExtendStylesMode = {
+    XmlTag: "XmlTag",
+    PStyle: "PStyle"
+}
+
+var QCharacterStyleAttributes = [
+    {
+        name: "appliedFont",
+        type: QType.Font
+    },
+    {
+        name: "fillColor",
+        type: QType.Color
+    },
+    {
+        name: "fontStyle",
+        type: QType.FontStyle
+    },
+    {
+        name: "pointSize",
+        type: QType.PointSize
+    },
+    {
+        name: "strikeThru", //Line through the text
+        type: QType.Bool
+    },
+    {
+        name: "underline",
+        type: QType.Bool
+    }
+];
+
+QParagraphStyleAttributes = [
+    //{
+//    name: "",
+//        type: QType.
+//},
+];
+QParagraphStyleAttributes = QParagraphStyleAttributes.concat(QCharacterStyleAttributes);
+
+var QPubConfig = {
+    documentPreferencesAttributes: [
+        {
+            name: "pageHeight",
+            type: QType.Size
+        },
+        {
+            name: "pageWidth",
+            type: QType.Size
+        }
+    ],
+    pageMarginsAttributes: [
+        {
+            name: "top",
+            type: QType.Size
+        },
+        {
+            name: "bottom",
+            type: QType.Size
+        },
+        {
+            name: "inside",
+            type: QType.Size
+        },
+        {
+            name: "outside",
+            type: QType.Size
+        }
+    ]
+};
